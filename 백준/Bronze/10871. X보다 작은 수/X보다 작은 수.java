6a2a9e1f961c265758c259c15a1381cbf4e7a1ab
Yeonjin Lee <130565870+Lee-yeonjin@users.noreply.github.com>
2024-12-06 16:03:32 +0900

    [Bronze V] Title: X보다 작은 수, Time: 148 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -12,13 +12,19 @@ public class Main{
         String inputLine = br.readLine();
         String[] num = inputLine.split(" ");
 
+        int n = Integer.parseInt(num[0]);
+        int m = Integer.parseInt(num[1]);
+
         String inputLine2 = br.readLine();
         String[] nums = inputLine2.split(" ");
 
-        for(int i=0; i<Integer.valueOf(num[0]); i++){
-            if(Integer.valueOf(nums[i]) < Integer.valueOf(num[1])) bw.write(nums[i] + " ");
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0; i<n; i++){
+            if(Integer.valueOf(nums[i]) < m) result.append(nums[i]).append(" ");
         }
 
+        bw.write(result.toString().trim());
         bw.flush();
         bw.close();
     }
