2d4a0e3aa5a63865b452fa617522c0c758f40370
Yeonjin Lee <130565870+Lee-yeonjin@users.noreply.github.com>
2025-05-09 12:59:02 +0900

    [Silver IV] Title: 스택 2, Time: 960 ms, Memory: 309028 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MyStack stack = new MyStack();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<num; i++){
+            String s = br.readLine();
+            if(s.length() >=2) {
+                String[] strs = s.split(" ");
+                stack.push(Integer.parseInt(strs[1]));
+            }
+            else if(s.equals("2")) sb.append(stack.pop()).append("\n");
+            else if(s.equals("3")) sb.append(stack.size()).append("\n");
+            else if(s.equals("4")) sb.append(stack.isEmpty()).append("\n");
+            else if(s.equals("5")) sb.append(stack.peek()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class MyStack {
+    private Stack<Integer> stack = new Stack<>();
+
+    public void push(int num){
+        stack.push(num);
+    }
+
+    public int pop(){
+        if(stack.isEmpty()) return -1;
+        return stack.pop();
+    }
+
+    public int size(){
+        return stack.size();
+    }
+
+    public int isEmpty(){
+        if(stack.isEmpty()) return 1;
+        else return 0;
+    }
+
+    public int peek(){
+        if(stack.isEmpty()) return -1;
+        return stack.peek();
+    }
+}
